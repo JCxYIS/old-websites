7f9cc59991ff5b71305e06c9056ce62d099c3bfb
JCxYIS <jsn811@gmail.com>
2017-03-24 23:22:27 +0800

    V.2.5

--- js/JCYISintro.js
+++ js/JCYISintro.js
@@ -1,3 +1,16 @@
 $(window).load(function(){
         $('#loading').fadeOut(777);
         });
+
+var width = window.screen.width;
+var height = window.screen.height;
+
+function adapt_rhombus(){
+	var size;
+	if(width>height){size = height/10;}
+	else {size = width/10;}
+	
+
+	document.getElementsByName('rhombus').style.color = "blue";
+	setTimeout(adapt_rhombus,10);
+}
