fc43fa74d32cee8d572ce2a06ee48dfccb974af7
JCxYIS <jsn811@gmail.com>
2017-04-01 16:11:07 +0800

    V.2.6

--- js/JCYISintro.js
+++ js/JCYISintro.js
@@ -1,16 +1,27 @@
 $(window).load(function(){
-        $('#loading').fadeOut(777);
+        load_complete();
         });
 
-var width = window.screen.width;
-var height = window.screen.height;
 
-function adapt_rhombus(){
-	var size;
-	if(width>height){size = height/10;}
-	else {size = width/10;}
-	
+function load_complete(){
+	$('#loading').fadeOut(777);
+	setTimeout(pull_header, 333);
+}
 
-	document.getElementsByName('rhombus').style.color = "blue";
-	setTimeout(adapt_rhombus,10);
+function pull_header(){
+	$(document).ready(function(){
+  		$('#header').slideDown(1234);
+  });	
+}
+
+function time(){
+	var today=new Date();
+	var hour = today.getHours(); if(hour<10){hour='0'+hour}
+	var min = today.getMinutes();if(min<10){min='0'+min}
+	var sec = today.getSeconds();if(sec<10){sec='0'+sec}
+	var nowday = today.getFullYear() + '/' + (today.getMonth()+1) + '/' + today.getDate() ;
+	var nowtime = hour + ':' + min + ':' + sec;
+
+	document.getElementById("time").innerHTML= nowday + '  ' + nowtime
+	setTimeout(time,10);
 }
